5f27940df01440a7bb2b8c169fceb851c1811f95
KingAbby200 <139162103+KingAbby200@users.noreply.github.com>
2025-11-12 00:58:49 +0100

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import { Switch, Route } from "wouter";
+import { Switch, Route, useLocation } from "wouter";
 import { useState, useEffect } from "react";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { queryClient } from "./lib/queryClient";
