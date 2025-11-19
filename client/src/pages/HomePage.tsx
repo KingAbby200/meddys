b752312ccf02b218f8da256621a166c7a658a247
KingAbby200 <139162103+KingAbby200@users.noreply.github.com>
2025-11-19 03:32:59 +0100

    Update HomePage.tsx

--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -35,7 +35,7 @@ export default function HomePage({ carouselImages, reviews }: HomePageProps) {
         />
         
         <div className="relative z-10 text-center px-4 sm:px-6 max-w-4xl mx-auto py-20">
-          <h1 className="font-poppins font-extrabold text-4xl sm:text-5xl lg:text-7xl text-yellow-500 mb-6 leading-tight animate-fadeIn">
+          <h1 className="font-poppins font-extrabold text-6xl sm:text-6xl lg:text-7xl text-primary border-white border-2 md:border-4 mb-6 leading-tight animate-fadeIn">
             Crave It. Taste It. Love It.
           </h1>
           <p className="text-base sm:text-lg text-white/90 mb-8 max-w-2xl mx-auto animate-fadeIn animation-delay-300">
