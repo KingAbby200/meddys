feaec557ea3d8b076cbc2079c00abb1dfb297581
KingAbby200 <139162103+KingAbby200@users.noreply.github.com>
2025-11-19 10:42:33 +0100

    Update HomePage.tsx

--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -29,7 +29,7 @@ export default function HomePage({ carouselImages, reviews }: HomePageProps) {
         <div
           className="absolute inset-0 bg-cover bg-center animate-zoom"
           style={{ 
-            backgroundImage: `url(/hero/lobby.jpg)`, 
+            backgroundImage: `url(/hero/wooden-table.jpg)`, 
             backgroundColor: '#111827'  // Dark fallback color
           }}
         />
